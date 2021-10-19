1b156a142106f7bd8e3a0e28146c6f0f62fef437
Renata Nunes <renatanunes231@yahoo.com.br>
2021-10-19 18:39:21 -0300

    Desafios 11, 12 e 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,16 @@
+use("aggregations");
+db.trips.aggregate([
+  { $match: {
+    startTime: { $gte: ISODate("2016-03-10") },
+  } },
+  { $group: {
+    _id: "null",
+    duracaoMediaEmMinutos: { $avg: {
+      $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60],
+    } },
+  } },
+  { $project: {
+    _id: 0,
+    duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+  } },
+]);
